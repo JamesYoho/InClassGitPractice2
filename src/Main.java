65d0f996b09e831ddfc11f94b3c56ecb2936d948
JamesYoho13 <YohoJR23@GCC.edu>
2026-02-09 13:23:08 -0500

    more changes after branching

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("First change");
-        System.out.println("Another change");
+        System.out.println("A different change");
     }
 }
